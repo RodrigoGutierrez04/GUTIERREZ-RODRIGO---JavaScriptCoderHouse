b18981ab2b5cee790cd2866d6ec09963983ef700
Rodrigo Gutierrez <rodrigogutierrezh4@gmail.com>
2022-07-31 16:19:02 -0500

    Nuevo Proyecto cargado y pusheado

--- js/javascript.js
+++ js/javascript.js
@@ -1,35 +1,52 @@
-let ingresoGasto = parseInt(prompt("Si vas a registar ingresos, escribe 1.\nSi vas a registar gastos, escribe 2.\nEscribe 3 para salir."))
-let dineroIngresado = 0
-let dineroGastado = 0
-let balance = 0
+const productosPrecios = [{ precio: 50, producto: "Parlante 1" },
+{ precio: 10,  producto: "Parlante 2" },
+{ precio: 100,  producto: "Parlante 3" }];
 
-function sumarIngresos(){
-    balance=balance + dineroIngresado
+// Filtrar por precios
+const filtro = productosPrecios.filter((el) => el.precio <= 50)
+
+const productos = ["undefined", "Parlante 1", "Parlante 2", "Parlante 3"];
+
+const precios = [undefined,50,10,100]
+
+let productosCarrito = parseInt(prompt("¿Cuál de los siguientes productos vas a llevar?\nEscribe " + productos.indexOf("Parlante 1") + " para " + productos[1] + " (" +precios[1] + " USD)\nEscribe " + productos.indexOf("Parlante 2") + " para " + productos[2] + " (" +precios[2] + " USD)\nEscribe " + productos.indexOf("Parlante 3") + " para " + productos[3] + " (" +precios[3] + " USD)\nEscribe 4 para mostrar EN CONSOLA los productos no mayores que 50 USD"))
+
+let subtotal = 0
+
+function sumarPrecio(){
+    subtotal=subtotal + precios[productosCarrito]
 }
 
-function restarGastos(){
-    balance=balance - dineroGastado; 
 
-}
-while(ingresoGasto!=3){
-    switch(ingresoGasto){
+while(productosCarrito!=5){
+    switch(productosCarrito){
         case 1:
-            dineroIngresado = parseInt(prompt("Registra tu ingreso (ejemplo: 1500 , 1300)"));
-            sumarIngresos();
+            alert("Se agregó " +  productos[productosCarrito] + " (" + precios[productosCarrito]+" USD) al carrito")
+            sumarPrecio()
             break;
-
         case 2:
-            dineroGastado = parseInt(prompt("Registra tu gasto (ejemplo: 1500 , 1300)"));
-            restarGastos();
+            alert("Se agregó " +  productos[productosCarrito] + " (" + precios[productosCarrito]+" USD) al carrito")
+            sumarPrecio()
+            break;
+        case 3:
+            alert("Se agregó " +  productos[productosCarrito] + " (" + precios[productosCarrito]+" USD) al carrito")
+            sumarPrecio()
+            break;
+        
+        case 4:
+            alert("Por favor, procede a escribir 5 y posteriormente, revisa la consola")
+            console.log(filtro)
             break;
 
-        default: 
-        alert("Opción no valida.")
-        break;
-    }
-    ingresoGasto = parseInt(prompt("Si vas a registar ingresos, escribe 1.\nSi vas a registar gastos, escribe 2.\nEscribe 3 para salir."))
-}
+        default:
+            alert("Opción no valida")
 
-alert("Tu balance es $" + balance)
+        }
+
+        productosCarrito = parseInt(prompt("¿Va a agregar otro producto?\nEscribe " + productos.indexOf("Parlante 1") + " para " + productos[1] + " (" +precios[1] + " USD)\nEscribe " + productos.indexOf("Parlante 2") + " para " + productos[2] + " (" +precios[2] + " USD)\nEscribe " + productos.indexOf("Parlante 3") + " para " + productos[3] + " (" +precios[3] + " USD)\nEscribe 4 para mostrar EN CONSOLA los productos no mayores que 50 USD\nEscribe 5 si no va a agregar ningún otro producto y mostrar su SUBTOTAL."))
+        }
+
+        alert("El subtotal es USD " + subtotal)
+
 
 
